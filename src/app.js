afcc3da0542a57ff457b2a46a57248c424eaa554
jathin_yendamuri <jathinyendamuri@gmail.com>
2024-12-23 01:34:47 +0530

    explored get,post,delete calles in postman

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ const server = require("express");
 
 const app = server();
 
-app.use("/home",(req,res)=>
+
+app.post("/hello",(req,res)=>
 {
-    res.send("hello from the server..");
-});
+    console.log("Saved data to the database."); //logic to save data in db.
+    res.send("data saved to DB (iam a post call)");
+
+})
+app.get("/hello",(req,res)=>
+    {
+        res.send("hello hello ..! , home.(get)");
+    })
+
+app.delete("/hello",(req,res)=>
+{
+    // code to delete data
+    res.send("data deleted..");
+})
+    
+app.use("/hello",(req,res)=>  // if we write this code at starting always the res will be "hello from server.." because it does not allow the remaining methods to exexute as the order matters here..
+    {
+        res.send("hello from the server..");
+    });
+
 
 app.listen(3000,()=>{
     console.log("server is running at post 3000");
